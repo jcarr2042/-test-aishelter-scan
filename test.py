d121646b1949ac5a81ac11e6ef9d5b41ebdd2bb7
jcarr2042 <jeremy.carr@aishelter.com>
2025-04-27 12:33:53 -0400

    Create test.py

---
+++ test.py
@@ -0,0 +1,7 @@
+API_KEY = "sk_test_51MabcDEF1234567890exampleSecretKey"
+
+def connect_to_service():
+    print("Connecting with API key:", API_KEY)
+
+if __name__ == "__main__":
+    connect_to_service()
