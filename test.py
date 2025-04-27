788dc45e44e9d442488299bd49b11c9cfe0eae9e
jcarr2042 <jeremy.carr@aishelter.com>
2025-04-27 13:46:47 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,4 @@
+import subprocess
+subprocess.call('ls', shell=True)  # <- Dangerous: CodeQL flags "Command Injection Risk"
+
 API_KEY = "sk_test_51MabcDEF1234567890exampleSecretKey"
-
-def connect_to_service():
-    print("Connecting with API key:", API_KEY)
-
-if __name__ == "__main__":
-    connect_to_service()
