c658f62208e29ab33af8b9b442ea54e2d34b67dc
jcarr2042 <jeremy.carr@aishelter.com>
2025-04-27 13:54:19 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,11 @@
 import subprocess
+import pickle
 subprocess.call('ls', shell=True)  # <- Dangerous: CodeQL flags "Command Injection Risk"
 
 API_KEY = "sk_test_51MabcDEF1234567890exampleSecretKey"
+
+# Insecure Deserialization - Don't use this in production
+data = b"cos\nsystem\n(S'ls'\ntR."
+# Insecurely loading the data
+loaded_data = pickle.loads(data)
+print(loaded_data)
